de86ddc5d43d1be13ef0c2b705a4cec063e3178d
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-02-05 01:03:31 +0530

    Initial Code with Movies & Actors
    - find & findone endpoints of Movies & Actors

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '9bbe131a601a12a44657f3357c54056c'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '12df68fc-a4bd-4e15-b562-d89c78876684'
+};
